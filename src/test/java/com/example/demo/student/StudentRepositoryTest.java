46d63be38f131589f388e0b34d80d2fbfcbe02ad
ktynor <kt9@tlen.pl>
2021-11-10 20:06:43 +0000

    itShouldCheckIfStudentExistsEmail

---
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository underTest;
+
+    @Test
+    void itShouldCheckIfStudentExistsEmail() {
+        String email = "jamila@gmail.com";
+        Student student = new Student(
+                "Jamila",
+                email,
+                Gender.FEMALE);
+        // given
+        underTest.save(student);
+        // when
+        boolean expected = underTest.selectExistsEmail(email);
+        // then
+        assertThat(expected).isTrue();
+
+    }
+}
