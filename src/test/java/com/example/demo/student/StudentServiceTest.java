bc907da17a703c81e7991ce5eb10568ca9afe736
ktynor <kt9@tlen.pl>
2021-11-13 20:03:37 +0000

    test with ArgumentCaptor

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,8 +34,24 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addStudent() {
+    void canAddStudent() {
+        //given
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE);
+        //when
+        underTest.addStudent(student);
+
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor =
+                ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
     }
 
     @Test
